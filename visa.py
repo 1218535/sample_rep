538d5ee7ea5b78d90dcf52056e070295693daea0
1218535 <1218535@ed.tus.ac.jp>
2019-07-09 14:21:49 +0900

    nakajima

--- visa.py
+++ visa.py
@@ -1,6 +1,11 @@
 #pyvisaしか入っていないので、それをvisaと直すのを忘れない。
 import pyvisa as visa
-rm = visa.ResourceManager('@py')
+rm = visa.ResourceManager()
+
+
+#まずは機械を物理的に接続する。そして、
+#機械のIPアドレス、品番？（キーサイトのホームページかなんかで調べる）
+#をかくにんして打ち込む。
 print(rm.list_resources())
 
 print(rm)
